efefb6f85f2394ebc5f3d50069ff925326623e30
torpilleur <nefdam@gmail.com>
2015-08-17 18:36:39 +0200

    Version hello world

---
+++ SNAP/MainWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SNAP
+{
+    /// <summary>
+    /// Logique d'interaction pour MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void click_me_button_Click(object sender, RoutedEventArgs e)
+        {
+            visual1.Text = "hello world";
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        private void Rectangle_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            this.DragMove();
+        }
+
+        private void slider_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            visual1.Text = slider.Value.ToString();
+        }
+
+        private void slider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            visual1.Text = slider.Value.ToString();
+        }
+    }
+}
