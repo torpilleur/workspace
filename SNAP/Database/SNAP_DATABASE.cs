d2cbd8feeb074a933750c12733b79ec56c68b96b
torpilleur <nefdam@gmail.com>
2016-08-20 12:34:15 +0200

    database complète + ihm partie

--- SNAP/Database/SNAP_DATABASE.cs
+++ SNAP/Database/SNAP_DATABASE.cs
@@ -1,6 +1,7 @@
 ﻿namespace SNAP.Database
 {
     using System;
+    using System.ComponentModel.DataAnnotations.Schema;
     using System.Data.Entity;
     using System.Linq;
 
@@ -20,6 +21,10 @@
         // Ajoutez un DbSet pour chaque type d'entité à inclure dans votre modèle. Pour plus d'informations 
         // sur la configuration et l'utilisation du modèle Code First, consultez http://go.microsoft.com/fwlink/?LinkId=390109.
         public virtual DbSet<Entity_joueurs> Table_Joueurs { get; set; }
+        public virtual DbSet<Entity_partie> Table_Parties { get; set; }
+        public virtual DbSet<Entity_trophes> Table_Trophes { get; set; }
+        public virtual DbSet<Entity_Classement_Nom> Table_Classement_Nom { get; set; }
+        public virtual DbSet<Entity_Occurence> Table_Occurence { get; set; }
         // public virtual DbSet<MyEntity> MyEntities { get; set; }
     }
 
@@ -32,5 +37,53 @@
         public string Arme_primaire { get; set; }
         public string Arme_secondaire { get; set; }
         public string Profil { get; set; }
+        public int? Nombre_kill_tot { get; set; }
+        public int? Nombre_death_tot { get; set; }
+        public int? Nombre_assistance_tot { get; set; }
+        public float? Ratio_tot { get; set; }
+        public string Classement { get; set; }
+        public int? Nombre_de_point { get; set; }
+        public int? Nombre_de_participation { get; set; }
+        public string Nom_classement { get; set; }
+
+    }
+    public class Entity_partie
+    {
+        public int ID { get; set; }
+
+        public string Nom { get; set; }
+        public string Date { get; set; }
+        public int? Nombre_de_participant { get; set; }
+        
+
+    }
+    public class Entity_trophes
+    {
+        public int ID { get; set; }
+
+        public string Nom { get; set; }
+        public string Condition_attribution { get; set; }
+
+    }
+    public class Entity_Classement_Nom
+    {
+        public int ID { get; set; }
+
+        public string Nom { get; set; }
+        public int? Nombre_point_ref { get; set; }
+       
+    }
+    public class Entity_Occurence
+    {
+        public int ID { get; set; }
+
+        public int?  Partie_ID { get; set; }
+        public int? Joueurs_ID { get; set; }
+        public int? Trophe_ID { get; set; }
+        public int? Nombre_kill { get; set; }
+        public int? Nombre_death { get; set; }
+        public int? Nombre_assist { get; set; }
+
+
     }
 }
